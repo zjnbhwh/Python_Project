1b06d705dfc6c75e5c7db914245f0e496f65e957
何文辉 <hewenhui@sinatay.com>
2018-04-12 10:30:43 +0800

    新增邮件系统

--- UM/Git_UM.py
+++ UM/Git_UM.py
@@ -9,20 +9,21 @@ import json
 
 
 
-class Git_Identity(ABCRelease.ABCRelease):
+class Git_UM(ABCRelease.ABCRelease):
     def __init__(self):
         cf = configparser.ConfigParser()
         cf.read("../resource/system.ini")
-        self.class_dir = cf.get(r"%s" % "identity", "class_dir")
-        super(Git_Identity, self).__init__("identity")
+        self.class_dir = cf.get(r"%s" % "um", "class_dir")
+        super(Git_UM, self).__init__("um")
 
     def get_diff_file(self, min, max):
         self.change = []
         os.chdir(self.base_dir)
-        os.system("git pull")
+        #os.system("git reset --hard HEAD")
+        os.system("git reset --hard HEAD")
         # changes = os.system("git diff %s %s --name-only|xargs zip update.zip " % (min, max))
         out = os.popen(
-            "git diff --binary %s %s --name-only ./dubbo/com.sinatay.dubbo.frame.identity " % (min, max))
+            "git diff --binary %s %s --name-only ./um/um" % (min, max))
         for i in out.readlines():
             filename = i.strip('\n')
             self.logger.info("文件" + filename + "下载到工程")
@@ -38,13 +39,14 @@ class Git_Identity(ABCRelease.ABCRelease):
 
     def __get_git_release(self):
         release = []
+
         for f in self.change:
-            if f.startswith("dubbo/com.sinatay.dubbo.frame.identity/JavaSource/java"):
+            print(f)
+            if f.startswith("um/um/JavaSource/src"):
                 if f.endswith(".java"):
                     f = f.replace(".java", ".class", 1)
-                f = f.replace("dubbo/com.sinatay.dubbo.frame.identity/JavaSource/java", "")
-                #print(f)
-                sourceFile = r"%s%s%s" % (self.base_dir, "dubbo/com.sinatay.dubbo.frame.identity/build/" + self.class_dir, f)
+                f = f.replace("um/um/JavaSource/src", "")
+                sourceFile = r"%s%s%s" % (self.base_dir, "um/um/build/" + self.class_dir, f)
                 targetDir = self.release_dir + self.date_file + "/WEB-INF/classes" + f[0:f.rfind("/")]
                 targetFile = targetDir + f[f.rfind("/"):]
 
@@ -54,11 +56,9 @@ class Git_Identity(ABCRelease.ABCRelease):
                     self.logger.info("将源文件" + sourceFile + "提取到：" + targetFile)
                     file.write(open(sourceFile, 'rb').read())
                 release.append(targetFile)
-            elif f.startswith("dubbo/com.sinatay.dubbo.frame.identity/JavaSource/resources"):
-                    f = f.replace("dubbo/com.sinatay.dubbo.frame.identity/JavaSource/resources", "")
-
-                    sourceFile = r"%s%s%s" % (
-                    self.base_dir, "dubbo/com.sinatay.dubbo.frame.identity/build/" + self.class_dir, f)
+            elif f.startswith("um/um/JavaSource/resource"):
+                    f = f.replace("um/um/JavaSource/resource", "")
+                    sourceFile = r"%s%s%s" % (self.base_dir, "um/um/build/" + self.class_dir, f)
                     targetDir = self.release_dir + self.date_file + "/WEB-INF/classes" + f[0:f.rfind("/")]
                     targetFile = targetDir + f[f.rfind("/"):]
                     if not os.path.exists(targetDir):
@@ -67,10 +67,11 @@ class Git_Identity(ABCRelease.ABCRelease):
                         self.logger.info("将源文件" + sourceFile + "提取到：" + targetFile)
                         file.write(open(sourceFile, 'rb').read())
                     release.append(targetFile)
-            elif f.startswith("dubbo/com.sinatay.dubbo.frame.identity/WebContent/WEB-INF"):
+            elif f.startswith("um/um/WebContent"):
                 sourceFile = r"%s%s" % (self.base_dir, f)
-                # 截掉identity/WebRoot目录
-                f = f.replace("dubbo/com.sinatay.dubbo.frame.identity/WebContent", "")
+                # 截掉um/WebRoot目录
+                f = f.replace("um/um/WebContent", "")
+                print("WebContent:"+f)
                 targetDir = self.release_dir + self.date_file + f[0:f.rfind("/")]
                 targetFile = targetDir + f[f.rfind("/"):]
                 if not os.path.exists(targetDir):
@@ -85,7 +86,7 @@ class Git_Identity(ABCRelease.ABCRelease):
     def build(self):
         os.chdir("E:/工作/project/Python_Project/resource")
         os.system("java -version")
-        os.system("ant -f %s" % "identity_build.xml")
+        os.system("ant -f %s" % "um_build.xml")
 
     def buildFileGroup(self, releaseFiles, replace_path):
         files = []
@@ -97,40 +98,33 @@ class Git_Identity(ABCRelease.ABCRelease):
 
 
 if __name__ == "__main__":
-    identity = Git_Identity()
-    identity.logger.info("####################开始########################" + identity.date_file)
-    min = "697eb282a92d6899b3b3592d8f90772c14829873"
-    max = "4aa8dad46bf0bbc4c0c77a6ab3677885a8e51a31"
-    identity.logger.info("始版本号：" + min)
-    identity.logger.info("终止版本号：" + max)
-    identity.get_diff_file(min, max)
+    um = Git_UM()
+    um.logger.info("####################开始########################" + um.date_file)
+    min = "fe9ee82bf2559b8d93a975a9c148dd92e70a3895"
+    max = "da122f379096261de79f205c5cf25c6535f5d040"
+    um.logger.info("始版本号：" + min)
+    um.logger.info("终止版本号：" + max)
+    um.get_diff_file(min, max)
     # 编译工程
-    identity.logger.info("编译工程")
-    identity.build()
+    um.logger.info("编译工程")
+    um.build()
     # 获得编2222译后的发布文件
-    #identity.logger.info("获得编译后的发布文件")
-    releaseFiles = identity.get_release()
-    replace_path = "/app/new_release/identity/"
-    files = identity.buildFileGroup(releaseFiles, replace_path)
+    #um.logger.info("获得编译后的发布文件")
+    releaseFiles = um.get_release()
+    replace_path = "/app/new_release/um/"
+    files = um.buildFileGroup(releaseFiles, replace_path)
     # 服务器配置
-    with open('E:/工作/project/Python_Project/resource/identity_servers.json') as json_file:
+    with open('E:/工作/project/Python_Project/resource/um_servers.json') as json_file:
         configs = json.load(json_file)
     for config in configs:
-        identity.logger.info("开始上传发布包到服务器：" + config["ip"])
+        um.logger.info("开始上传发布包到服务器：" + config["ip"])
         ssh = Ssh.SshUtil(config["ip"], config["port"], config["username"], config["password"])
         ssh.sftp_uploadFile(files, config["mkdirs"])
-        identity.logger.info("将服务器上的发布包发布到项目，并备份")
-        ssh.release(config["release"], identity.date_file)
+        um.logger.info("将服务器上的发布包发布到项目，并备份")
+        ssh.release(config["release"], um.date_file)
         ssh.ssh.close()
-        identity.logger.info("上传服务器：" + config["ip"] + "结束")
+        um.logger.info("上传服务器：" + config["ip"] + "结束")
 
-    identity.logger.info("####################结束########################" + identity.date_file)
+    um.logger.info("####################结束########################" + um.date_file)
 
-if __name__ == "__main2__":
-    identity = Git_Identity()
-    identity.logger.info("####################开始########################" + identity.date_file)
-    min = "xxxxcc22222222rrcx"
-    max = "xxxxcccx"
-    identity.logger.info("始版本号：" + min)
-    identity.logger.info("终止版本号：" + max)
-    identity.get_diff_file(min, max)
+
---
+++ xtmail/SVN_MI.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#功能：从svn抓发布包，本地编译后，增量发布到服务器
+#
+
+__author__ = 'hewenhui'
+from base import ABCRelease
+from base import Ssh
+import configparser
+import pysvn
+from urllib import parse
+import os
+import json
+
+
+class XtMail(ABCRelease.ABCRelease):
+    def __init__(self):
+        cf = configparser.ConfigParser()
+        cf.read("../resource/system.ini")
+        #本地编译后的class路径
+        self.class_dir = cf.get(r"%s" % "xtmail", "class_dir")
+        super(XtMail, self).__init__("xtmail")
+    #该方法目前只处理了新增或修改的发布
+    def get_diff_file(self, min, max):
+        targetPath = self.release_svn_dir + self.date_file
+        client = pysvn.Client()
+        revision_max = pysvn.Revision(pysvn.pysvn.opt_revision_kind.number, max)
+        revision_min = pysvn.Revision(pysvn.pysvn.opt_revision_kind.number, min)
+        self.change = client.diff_summarize(self.url, revision_min, self.url, revision_max)
+        for changed in self.change:
+            try:
+                if pysvn.diff_summarize_kind.added == changed['summarize_kind'] or pysvn.diff_summarize_kind.modified == \
+                        changed['summarize_kind']:
+                    self.logger.info("从SVN下载：" + changed['path'])
+                    file_text = client.cat(self.url + parse.quote(changed['path'].encode('utf8')), revision_max)
+                    fullpath = targetPath + "/" + changed["path"]
+                    dirpath = fullpath[0:fullpath.rfind("/")]
+                    if not os.path.exists(dirpath):
+                        os.makedirs(dirpath)
+                    with open(fullpath, "wb") as f:
+                        f.write(file_text)
+            except:
+                print("error:" + parse.quote(changed['path'].encode('utf8')))
+                continue
+        return targetPath
+
+    def get_release(self):
+        self.logger.info("将编译后的文件，按差异提取到：" + self.release_dir)
+        release = []
+        for changed in self.change:
+            if pysvn.diff_summarize_kind.added == changed['summarize_kind'] or pysvn.diff_summarize_kind.modified == \
+                    changed['summarize_kind']:
+                f = changed["path"]
+                if f.startswith("mi/src/test"):
+                    continue
+                if f.startswith("mi/src/main"):
+
+                    if f.endswith(".java"):
+                        f = f.replace("mi/src/main/java", "")
+                        temp = f[f.rfind("/") + 1:]
+                        className = temp[0:temp.rfind(".")]
+                        pathName = f[0:f.rfind("/") + 1]
+                        pathName = r"%s%s%s" % (self.base_dir, self.class_dir, pathName)
+                        for f in self.get_classFile(pathName, className):
+                            filePath = f.replace("D:/source_project/MI/mi/webapp/WEB-INF/classes/", "")
+
+                            targetDir = self.release_dir + self.date_file + "/WEB-INF/classes/" + filePath[
+                                                                                                  0:filePath.rfind("/")]
+                            targetFile = targetDir + filePath[filePath.rfind("/"):]
+
+                            if not os.path.exists(targetDir):
+                                os.makedirs(targetDir)
+                            with open(targetFile, "wb") as file:
+                                try:
+                                    file.write(open(f, 'rb').read())
+                                except Exception as e:
+                                    print("错误："+f)
+                                    print(e)
+                            release.append(targetFile)
+                    else:
+                        if f.startswith("mi/src/main/java"):
+                            f = f.replace("mi/src/main/java", "")
+                        else:
+                            f = f[f.rfind("/"):]
+
+                        sourceFile = r"%s%s%s" % (self.base_dir, self.class_dir, f)
+
+                        if os.path.isfile(sourceFile):
+                            targetDir = self.release_dir + self.date_file + "/WEB-INF/classes" + f[0:f.rfind("/")]
+
+                            targetFile = targetDir + f[f.rfind("/"):]
+                            if not os.path.exists(targetDir):
+                                os.makedirs(targetDir)
+                            with open(targetFile, "wb") as file:
+                                file.write(open(sourceFile, 'rb').read())
+                            release.append(targetFile)
+                else:
+                    sourceFile = r"%s%s" % (self.base_dir, f)
+                    if os.path.isfile(sourceFile):
+                        # 截掉Web Root目录
+                        f = f.replace("mi/webapp", "")
+                        targetDir = self.release_dir + self.date_file + f[0:f.rfind("/")]
+                        targetFile = targetDir + f[f.rfind("/"):]
+                        if not os.path.exists(targetDir):
+                            os.makedirs(targetDir)
+                        with open(targetFile, "wb") as file:
+                            file.write(open(sourceFile, "rb").read())
+                        release.append(targetFile)
+        return release
+
+    def build(self):
+        os.chdir("E:/工作/project/Python_Project/resource")
+        os.system("ant -f %s" % "mi_build.xml")
+
+    def buildFileGroup(self, releaseFiles):
+        files = []
+        for f in releaseFiles:
+            files.append({"sourceFile": f,
+                          "targetFile": f.replace(self.release_dir + self.date_file,
+                                                  "/app/new_release/mi/" + self.date_file)})
+
+        return files
+
+
+if __name__ == "__main__":
+    xtmail = XtMail()
+    xtmail.logger.info("####################开始########################" + xtmail.date_file)
+    xtmail.update()
+    min = "2254"
+    max = "2260"
+    xtmail.logger.info("始版本号：" + min)
+    xtmail.logger.info("终止版本号：" + max)
+    # 获得svn文件,并返回存放路径
+    #svnPath = mi.get_diff_file(min, max)
+
+    xtmail.logger.info("####################结束#########################" + xtmail.date_file)
+
+if __name__ == "__main1__":
+    mi = XtMail()
+    mi.logger.info("####################开始########################" + mi.date_file)
+    mi.update()
+    min = input("请输入起始版本号：")
+    max = input("请输入终止版本号：")
+
+    mi.logger.info("始版本号：" + min)
+    mi.logger.info("终止版本号：" + max)
+    # 获得svn文件,并返回存放路径
+    svnPath = mi.get_diff_file(min, max)
+    #将svn文件拷贝到工程并备份
+    mi.copyFiles(svnPath, mi.base_dir)
+    #编译工程
+    mi.build()
+    #按差异将编译后的文件拷贝到发布目录
+    releaseFiles = mi.get_release()
+    #组织发布到服务器的二维数组
+    files = mi.buildFileGroup(releaseFiles)
+    #读取服务器配置
+    with open('E:/工作/project/Python_Project/resource/mi_servers.json') as json_file:
+        configs = json.load(json_file)
+    for config in configs:
+        mi.logger.info("开始上传发布包到服务器：" + config["ip"])
+        ssh = Ssh.SshUtil(config["ip"], config["port"], config["username"], config["password"])
+        ssh.sftp_uploadFile(files, config["mkdirs"])
+        mi.logger.info("将服务器上的发布包发布到项目，并备份")
+        ssh.release(config["release"], mi.date_file)
+        ssh.ssh.close()
+        mi.logger.info("上传服务器：" + config["ip"] + "结束")
+
+    mi.logger.info("####################结束#########################" + mi.date_file)
